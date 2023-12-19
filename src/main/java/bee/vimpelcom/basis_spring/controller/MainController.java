e7af2cb30ec90835cc97a88f76f5a53fd80674b3
m.moiseev <iammoiseev@gmail.com>
2023-12-19 23:21:57 +0300

    inn

---
+++ src/main/java/bee/vimpelcom/basis_spring/controller/MainController.java
@@ -0,0 +1,67 @@
+package bee.vimpelcom.basis_spring.controller;
+
+
+import bee.vimpelcom.basis_spring.zabbix.ZabbixAPI;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Controller
+public class MainController {
+
+    //final static Logger logger = Logger.getLogger(MainController.class);
+    final static Logger logger = LoggerFactory.getLogger(MainController.class);
+
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("message","hello world");
+        logger.info("Get index");
+        return "index";
+    }
+//
+    @GetMapping("/d")
+    public String dashboardIn(Model model) throws IOException {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
+        ZabbixAPI.updateAuthToken();
+        //String[][] dtaexp = {"1","2"},{"1","2"};
+        model.addAttribute("dateTime", LocalDateTime.now().format(dateTimeFormatter));
+        model.addAttribute("percent_errors", ZabbixAPI.getProcErrors()); // процент ошибочных
+        model.addAttribute("success_count", ZabbixAPI.getSuccessActions()); // ecgtiyst
+        model.addAttribute("zakupki_status", ZabbixAPI.getZakupkiStatus()); // ecgtiyst
+        model.addAttribute("users_count", ZabbixAPI.getUsersCount()); // ecgtiyst
+
+        model.addAttribute("all_inc",ZabbixAPI.getAllInc());
+        model.addAttribute("incoming_inc",ZabbixAPI.getIncomingIncToday());
+        model.addAttribute("done_inc",ZabbixAPI.getDoneIncToday());
+        model.addAttribute("in_work_inc",ZabbixAPI.getInWorkInc());
+        model.addAttribute("wait_inc",ZabbixAPI.getWaitIncToday());
+
+
+        /*
+        resultMes.append(getAllInc()+"<br>");
+        resultMes.append(getIncomingIncToday()+"<br>");
+        resultMes.append(getDoneIncToday()+"<br>");
+        resultMes.append(getInWorkInc()+"<br>");
+        //resultMes.append(getActiveIncToday()+"<br>");
+        resultMes.append(getWaitIncToday()+"<hr>");
+        resultMes.append("<h3>ПРОСРОЧЕННЫЕ ИНЦИДЕНТЫ</h3>");
+        resultMes.append(getProsrokTable());
+         */
+
+
+
+        logger.info("Get dashboard");
+        return "dashboardIn";
+    }
+
+
+}
